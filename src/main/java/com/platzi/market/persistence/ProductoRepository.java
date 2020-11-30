8367fc77e66492c34acbb695bfabe42b2867b3af
Luis Guerra <lguerraluis@gmail.com>
2020-11-30 11:11:04 -0600

    Add @Repository to ProductRepository

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,14 +2,19 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 /**
  * Aqui se crea una clase a la altura de persistance
- * para poder crear los metodos que tiene el crudRepository
+ * para poder crear los metodos que tiene el crudRepository,
+ * ademas le anadimos repository decorator para indicarle
+ * a Spring que la clase interactua con  la bd-> CRUD
  */
+
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -23,5 +28,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
